b8cbc5ebba8ab51835366266c79e9e691b61ca3a
TimotheeChauvin <timothee.chauvin@orange.fr>
2019-02-09 20:37:19 +0100

    add chap2p2

---
+++ chap2p2/exec_normal.py
@@ -0,0 +1,5 @@
+import mnist_loader
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+import network
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
