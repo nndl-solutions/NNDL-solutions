9c0d2e59c8ad07ddf81b5a4f4759878a0d956352
TimotheeChauvin <timothee.chauvin@orange.fr>
2019-02-24 21:02:52 +0100

    add chap3 ex9

---
+++ chap3ex9/exec_ls.py
@@ -0,0 +1,13 @@
+import mnist_loader
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+import network2_ls
+net = network2_ls.Network([784, 30, 10], cost=network2_ls.CrossEntropyCost)
+net.SGD(training_data, 10, 0.5,
+        lmbda=5.0,
+        es=10,
+        evaluation_data=validation_data,
+        monitor_evaluation_accuracy=True,
+        monitor_evaluation_cost=False,
+        monitor_training_accuracy=False,
+        monitor_training_cost=False
+)
