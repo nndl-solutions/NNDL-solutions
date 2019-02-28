f1b4def5ac82cf108d8e7520edfad2bf77b26480
nndl-solutions <nndlsolutions@fastmail.com>
2019-02-28 15:20:33 +0100

    updates for theano 1.0.3

--- original-python-3/network3.py
+++ original-python-3/network3.py
@@ -39,7 +39,7 @@ import gzip
 import numpy as np
 import theano
 import theano.tensor as T
-from theano.tensor.nnet import conv
+from theano.tensor.nnet import conv2d
 from theano.tensor.nnet import softmax
 from theano.tensor import shared_randomstreams
 from theano.tensor.signal.pool import pool_2d
@@ -227,11 +227,11 @@ class ConvPoolLayer(object):
 
     def set_inpt(self, inpt, inpt_dropout, mini_batch_size):
         self.inpt = inpt.reshape(self.image_shape)
-        conv_out = conv.conv2d(
+        conv_out = conv2d(
             input=self.inpt, filters=self.w, filter_shape=self.filter_shape,
-            image_shape=self.image_shape)
+            input_shape=self.image_shape)
         pooled_out = pool_2d(
-            input=conv_out, ds=self.poolsize, ignore_border=True)            
+            input=conv_out, ws=self.poolsize, ignore_border=True)            
         self.output = self.activation_fn(
             pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
         self.output_dropout = self.output # no dropout in the convolutional layers
