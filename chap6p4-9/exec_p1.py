6c381c580524fb52c5c481345f1b6fb18b55b353
TimotheeChauvin <timothee.chauvin@orange.fr>
2019-03-01 14:18:09 +0100

    add chap6 p4

---
+++ chap6p4-9/exec_p1.py
@@ -0,0 +1,13 @@
+import network3p1
+from network3p1 import Network
+from network3p1 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
+training_data, validation_data, test_data = network3p1.load_data_shared()
+mini_batch_size = 10
+net = Network([
+        ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28), 
+                      filter_shape=(5, 1, 5, 5), 
+                      poolsize=(2, 2)),
+        FullyConnectedLayer(n_in=5*12*12, n_out=30),
+        SoftmaxLayer(n_in=30, n_out=10)], mini_batch_size)
+net.SGD(training_data, mini_batch_size, 0.1, 
+            validation_data, test_data, es=10)
